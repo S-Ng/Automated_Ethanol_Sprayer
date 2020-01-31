8cbc905e62161f81e712d257d5730bf996769e11
Natasha <60375503+nputhukudy@users.noreply.github.com>
2020-01-31 00:02:50 -0800

    Update IR_Servo.ino

--- IR_Servo.ino
+++ IR_Servo.ino
@@ -1,26 +1,30 @@
 #include <Servo.h>
 Servo myservo;  // create servo object to control a servo
-int pos = 0;	// variable to store the servo position
+int pos;  // variable to store the servo position
 
 void setup() {
   Serial.begin(9600);
-  pinMode('D8', INPUT); // set pin as input
-  myservo.attach('D4');
+  pinMode('8', INPUT); // set pin as input
+  myservo.attach('4');
 }
 void loop() {
   int detect = digitalRead(8); // read obstacle status and store it into "detect"
   if(detect == LOW){ // object present
-	for (pos = 0; pos <= 180; pos += 10) {
-	myservo.write(pos);
-	delay(15);
-	}
-	for (pos = 180; pos >= 0; pos -= 10) {
-	myservo.write(pos);
-	delay(15);
-	}
+    Serial.println("yo");
+    pos = 0;
+    for (pos = 0; pos <= 180; pos += 10) {
+      Serial.println(pos);
+      myservo.write(pos);
+      delay(15);
+    }
+    for (pos = 180; pos >= 0; pos -= 10) {
+      myservo.write(pos);
+      delay(15);
+    }
   }
   else{
+   pos = 180;
    Serial.println("All clear");  // do nothing
   }
-  delay(3000); // wait 3 seconds
+  delay(500); // wait 0.5 seconds
 }
